5754a56a7389668231af910aaa7f3b202766294f
Tuanhdhapo <tuan.hd@haposoft.com>
2021-08-20 11:12:02 +0700

    fix errors convention

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -193,21 +193,21 @@
                                             </div>
                                             @error('name')
                                             <div class=" alert-danger">{{ $message }}</div>
-                                        @enderror
+                                            @enderror
                                             <div class="input"> <label>Email:</label><br>
                                                 <input type="email" name="email_user"  class="@error('email_user') is-invalid @enderror"
                                                 ><br>
                                             </div>  
                                             @error('email_user')
                                             <div class=" alert-danger">{{ $message }}</div>
-                                        @enderror   
+                                            @enderror   
                                             <div class="input"> <label>Password:</label><br>
                                                 <input type="password" name="password_register" class="@error('password_register') is-invalid @enderror"
                                                ><br>
                                             </div>
                                             @error('password_register')
                                             <div class=" alert-danger">{{ $message }}</div>
-                                        @enderror
+                                            @enderror
                                             <div class="input"> <label>Repeat
                                                     Password:</label><br>
                                                 <input type="password" name="password_confirmation"  class="@error('password_confirmation') is-invalid @enderror"
@@ -215,7 +215,7 @@
                                             </div>
                                             @error('password_confirmation')
                                             <div class=" alert-danger">{{ $message }}</div>
-                                        @enderror    
+                                            @enderror    
                                             <p class="text-center">
                                                 <button
                                                     type="submit" class="btn
